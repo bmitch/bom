f10df7296ab160abce4029ee659994746a85e989
Bill Mitchell <wkmitch@gmail.com>
2023-08-10 19:19:37 -0700

    update

--- bom.c
+++ bom.c
@@ -5,6 +5,11 @@
 
 #define RED "\x1B[31m"
 #define RESET "\x1B[0m"
+#define WHITE "\x1B[37m"
+#define BLUE "\x1B[34m"
+#define CYAN "\x1B[36m"
+#define GREEN "\x1B[32m"
+#define RESET "\x1B[0m"
 
 typedef struct {
     char url[256];
@@ -15,11 +20,16 @@ typedef struct {
 
 void SaveBookmarkToFile(Bookmark bookmark);
 void addBookmark();
+void listBookmarks();
 
 int main(int argc, char *argv[]) {
     if (argc > 1 && strcmp(argv[1], "add") == 0) {
         addBookmark();
-    } else {
+    } else if (argc > 1 && strcmp(argv[1], "list") == 0) {
+        listBookmarks();
+    }
+
+    else {
         printf(RED "Unknown command." RESET "\n");
     }
 
@@ -63,4 +73,39 @@ void addBookmark() {
 
     SaveBookmarkToFile(newBookmark);
     printf("Bookmark saved successfully.\n");
+}
+
+void listBookmarks() {
+    FILE *file = fopen("bookmarks.txt", "r");
+    if (file == NULL) {
+        printf("Could not open bookmarks file.\n");
+        return;
+    }
+
+    char line[256];
+    int lineCount = 0;
+    while (fgets(line, sizeof(line), file)) {
+        // Remove newline character if it exists
+        line[strcspn(line, "\n")] = 0;
+
+        switch (lineCount % 4) {
+            case 0:
+                printf(WHITE "URL: %s\n" RESET, line);
+                break;
+            case 1:
+                printf(BLUE "Title: %s\n" RESET, line);
+                break;
+            case 2:
+                printf(CYAN "Tags: %s\n" RESET, line);
+                break;
+            case 3:
+                printf(GREEN "Date added: %s\n" RESET, line);
+                printf("\n"); // Separating bookmarks with a newline
+                break;
+        }
+
+        lineCount++;
+    }
+
+    fclose(file);
 }
